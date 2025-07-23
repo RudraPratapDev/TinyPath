114684e2e13fbd437d181e12c1b36aa6ee125f7e
RudraPratapDev <rudra.tomar608@gmail.com>
2025-07-23 10:11:55 +0530

    fix(auth): enable cross-domain authentication

--- FRONTEND/src/utils/axiosInstance.js
+++ FRONTEND/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const axiosInstance=axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: 'https://tinypath-production.up.railway.app',
     timeout:10000,
     withCredentials:true
 })
